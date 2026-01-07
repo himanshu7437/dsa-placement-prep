bdbc66ba5380ea86a677688cb08021b3b2951208
himanshu7437 <himanshujangra368@gmail.com>
2026-01-07 12:34:58 +0530

    Problem - palindrome check added

--- week-1/problemset/ProblemSet.java
+++ week-1/problemset/ProblemSet.java
@@ -5,7 +5,7 @@ public class ProblemSet {
         
 
     // ques - 1) Reverse String
-
+    // ques - 2) palindrome check
     }
 
     //soln1 
@@ -38,4 +38,39 @@ public class ProblemSet {
         }
     }
 
+
+    // soln2
+
+	public static boolean checkPalindrome(String str) {
+	    // WRITE YOUR CODE HERE	
+		if (str == null || str.length() == 0) return true;
+
+	 	int left = 0;
+		int right = str.length() - 1;
+
+		while(left < right) {
+
+			// check blank spaces and special character
+			while(left < right && !Character.isLetterOrDigit(str.charAt(left))) left++;
+			while(left < right && !Character.isLetterOrDigit(str.charAt(right))) right--;
+
+
+			// checking
+			if(Character.toLowerCase(str.charAt(left)) != Character.toLowerCase(str.charAt(right))) {
+				return false;
+			}
+
+			// pointer increment and decrment
+			left++;
+			right--;
+
+		}
+
+		return true;
+
+	}
+
+    
 }
+
+
